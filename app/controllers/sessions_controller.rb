1a78492dcb56913423a281935725979ee5e18749
Ryo <ryo.bbl9@gmail.com>
2019-12-22 13:03:29 +0900

    永続機能追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to root_url
     else
       flash.now[:danger] = '有効なメールアドレス、またはパスワードではありません。' # 本当は正しくない
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to login_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
 
     before_save :downcase_email
 
@@ -12,6 +13,31 @@ class User < ApplicationRecord
     has_secure_password
     validates :password,presence: true,length: { minimum: 6 },allow_nil:true
 
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+
+    #ランダムなトークンを返す
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    # 永続セッションのためにユーザーをデータベースに記憶する
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest,User.digest(remember_token))
+    end
+
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 
     private
 
