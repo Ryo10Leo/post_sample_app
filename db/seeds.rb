7480869ffc4188db51bdabe857a1b11923130d28
Ryo <ryo.bbl9@gmail.com>
2020-01-01 01:27:26 +0900

    Article 修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,32 +28,62 @@ Product.create(name: "なんかすごいトリートメント",
                 description: "気になる髪になんかすごい成分配合のナンチャラが効く！",
                 picture: File.open("#{Rails.root}/test/fixtures/product5.jpg"))
 
-  Style.create(caption: "カット&カラー",
+Style.create(caption: "カット&カラー",
                 price: "5000",
                 picture: File.open("#{Rails.root}/test/fixtures/model_sample1.jpg"))
 
-  Style.create(caption: "カット&カラー",
+Style.create(caption: "カット&カラー",
                 price: "5000",
                 picture: File.open("#{Rails.root}/test/fixtures/model_sample2.jpg"))
 
-  Style.create(caption: "カット&カラー",
+Style.create(caption: "カット&カラー",
                 price: "5000",
                 picture: File.open("#{Rails.root}/test/fixtures/model_sample3.jpg"))
 
-  Style.create(caption: "カット&カラー",
+Style.create(caption: "カット&カラー",
                 price: "5000",
                 picture: File.open("#{Rails.root}/test/fixtures/model_sample4.jpg"))
 
-  Style.create(caption: "カット&カラー",
+Style.create(caption: "カット&カラー",
                 price: "5000",
                 picture: File.open("#{Rails.root}/test/fixtures/model_sample5.jpg"))
 
-  Style.create(caption: "カット&カラー",
+Style.create(caption: "カット&カラー",
                 price: "5000",
                 picture: File.open("#{Rails.root}/test/fixtures/model_sample6.jpg"))
 
-   User.create(name: "Ryo Iwata", email: "ryo.bbl9@gmail.com",
+User.create(name: "Ryo Iwata", email: "ryo.bbl9@gmail.com",
             password: "mimuratoeregansu", password_confirmation: "mimuratoeregansu")
+
+Article.create(title:"髪の豆知識",
+               content: '<p><img alt="" src="/uploads/ckeditor/pictures/3/content_product5.jpg" /></p> <p>こちらがおすすめ！</p> <p>トリートメントは使いすぎ注意ですよ</p>')
+
+Article.create(title: "新しいシャンプー入荷しました！",
+               content: '<p><img alt="" src="/uploads/ckeditor/pictures/4/content_product6.jpg" /></p> <p>こちらXX社の新商品です！</p> <p>カラーをしていても、色落ちを抑えます！</p>')
+
+Article.create(title:"台風に伴う臨時休業のお知らせ",
+               content:'<p>&nbsp;</p> <p>&nbsp;</p> <p>レシオ美容室は台風〇〇号の接近に伴い、</p> <p>&nbsp;</p> <p>&nbsp;</p> <p>誠に勝手ながら本日から明後日〇〇日までの営業をお休みさせていただきます。</p> <p>&nbsp;</p> <p>&nbsp;</p> <p>&nbsp;</p>')
+
+Article.create(title:"学割キャンペーン実施中！",
+               content:'<p>&nbsp;</p> <p>&nbsp;</p> <p>みなさんこんにちわ！</p> <p>&nbsp;</p> <p>本日から〇〇月〇〇日まで高校生以下のお客様はお会計から10%割引いたします！</p> <p>&nbsp;</p> <p>ぜひレシオ美容室にお越しくださいませ。。。</p> <p>&nbsp;</p> <p>&nbsp;</p> <p>&nbsp;</p>')
+
+Article.create(title:"価格変更のお知らせ",
+               content:'<p>&nbsp;</p> <p>&nbsp;</p> <p>&nbsp;</p> <p>レシオ美容室は消費税増税に伴い、価格を一部変更いたします。</p> <p>&nbsp;</p> <p>これからもレシオ美容室をよろしくお願い申し上げます。。。</p> <p>&nbsp;</p> <p>&nbsp;</p> <p>&nbsp;</p>')
+
+Category.create(name:"商品")
+Category.create(name:"ratio.news")
+Category.create(name:"お知らせ")
+Category.create(name:"キャンペーン")
+
+ArticleCategory.create(article_id:2,category_id:1)
+ArticleCategory.create(article_id:2,category_id:2)
+ArticleCategory.create(article_id:2,category_id:3)
+ArticleCategory.create(article_id:3,category_id:3)
+ArticleCategory.create(article_id:4,category_id:2)
+ArticleCategory.create(article_id:4,category_id:3)
+ArticleCategory.create(article_id:4,category_id:4)
+ArticleCategory.create(article_id:5,category_id:2)
+ArticleCategory.create(article_id:5,category_id:3)
 
 
 
