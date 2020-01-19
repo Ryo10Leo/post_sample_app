ae7dde04b10d9d448d75a845549451db8ebed471
Ryo10Leo <ryo.bbl9@gmail.com>
2020-01-19 14:59:29 +0900

    修正

--- test/models/article_category_test.rb
+++ test/models/article_category_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ArticleCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # def setup
+  #   @articles_category = ArticleCategory.new(article_id: 1,
+  #   category_id: 2)
+  # end
+
+  # test "should be valid" do
+  #   assert @articles_category.valid?
+  # end
+
+  # test "should require a article_id" do
+  #   @articles_category.article_id = nil
+  #   assert_not @articles_category.valid?
+  # end
+
+  # test "should require a category_id" do
+  #   @articles_category.category_id = nil
+  #   assert_not @articles_category.valid?
   # end
 end
