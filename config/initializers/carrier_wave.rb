44e3eae3f2037d91b1889db1c19ecbde86626941
Ryo10Leo <ryo.bbl9@gmail.com>
2020-01-25 23:21:10 +0900

    修正

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,12 +3,11 @@ if Rails.env.production?
       config.fog_credentials = {
         # Amazon S3用の設定
         :provider              => 'AWS',
-        :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+        :region                => ENV['S3_REGION'],
         :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
         :aws_secret_access_key => ENV['S3_SECRET_KEY']
       }
       config.fog_directory     =  ENV['S3_BUCKET']
-      config.asset_host = 'https://static.salon-ratio.com'
     end
 
 end
